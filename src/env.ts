e108e2bf2fc7df01ab4bba379c438bf67bf3542e
Cedricpasquier <dedridec@gmail.com>
2023-03-01 16:20:17 -0500

    Auto-generated commit [ci skip]

--- src/env.ts
+++
@@ -1,3 +0,0 @@
-const env = {
-  API_URL: "https://portfolioweb-309920-default-rtdb.europe-west1.firebasedatabase.app/",
-};
--- src/environments/environment.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-  API_URL: "https://portfolioweb-309920-default-rtdb.europe-west1.firebasedatabase.app/",
-};
